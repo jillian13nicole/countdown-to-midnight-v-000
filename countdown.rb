e1dce2fcbd7606ed8a800af45d9c9fbe16690264
Jillian Weimer <jilliannweimer@gmail.com>
2020-07-15 19:55:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown
-  counnter = 10
-  while counnter > 0
+  counter = 10
+  while counter > 0
     puts "#{counter} SECOND(S)!"
-    counnter -= 1
+    counter -= 1
   end
 end
